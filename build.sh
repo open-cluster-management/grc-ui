741538ce2493e0f5bf9019183b63722a6d541a1c
ycao <ycao@us.ibm.com>
2020-02-05 10:41:12 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Run our build target and set IMAGE_NAME_AND_VERSION
-export IMAGE_NAME_AND_VERSION=${1}
+export DOCKER_IMAGE_NAME=${1}
 make build
 make lint
 make prune
+make docker/build
