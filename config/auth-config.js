3b2965d26b92e3307b3841056dc443f005ead17e
Yu Cao <ycao@us.ibm.com>
2020-02-10 20:00:04 -0500

    use env

--- config/auth-config.js
+++ config/auth-config.js
@@ -7,12 +7,10 @@ config.ocp.oauth2_clientid= process.env.OAUTH2_CLIENT_ID
 config.ocp.oauth2_clientsecret= process.env.OAUTH2_CLIENT_SECRET
 config.ocp.oauth2_redirecturl = process.env.OAUTH2_REDIRECT_URL
 
+config.ocp.apiserver_url=process.env.API_SERVER_URL
 if (process.env.NODE_ENV === 'development') {
-  config.ocp.apiserver_url=process.env.API_SERVER_URL
   config.ocp.serviceaccount_token=process.env.SERVICEACCT_TOKEN
 } else {
-  // SERVICE ACCOUNT TOKEN needed to review user token
-  config.ocp.apiserver_url='https://kubernetes.kube-system.svc'
   config.ocp.serviceaccount_token =  fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token').toString()
 }
 
