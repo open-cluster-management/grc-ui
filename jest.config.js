fdf157287e0f5b82bcb7f55ff1f1fd4208710499
Ray Harris <ray@harris.net>
2020-02-14 13:22:35 -0500

    sonar: add sonarcloud scanning

--- jest.config.js
+++ jest.config.js
@@ -30,6 +30,9 @@ const jestConfig = {
     'text',
     'text-summary'
   ],
+  testResultsProcessor: [
+    'jest-sonar-reporter',
+  ],
   testURL: 'http://localhost/',
   coverageThreshold: {
     global: {
