7c12cc2988ae0d8500744e8da860219ae7b3ee03
Xuning Kang <Xuning.Kang@ibm.com>
2020-03-12 22:07:06 +0800

    uesless change for test

--- tests/jest/containers/GrcRouter.test.js
+++ tests/jest/containers/GrcRouter.test.js
@@ -5,6 +5,8 @@
  * Note to U.S. Government Users Restricted Rights:
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
+ *******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
  *******************************************************************************/
 'use strict'
 
@@ -33,7 +35,7 @@ describe('Grc Router container test', () => {
       'path': '/multicloud',
       'url': '/multicloud',
       'isExact': false,
-      'params': {}
+      'params': {},
     }
     const component = shallow(
       <ApolloProvider client={apolloClient.getGrcClient()}>
