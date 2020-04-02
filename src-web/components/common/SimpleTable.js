dfd8d533d2f48d38846e2078dadd8aa82a3ccc23
Chunxi Luo <Chunxi.Luo@ibm.com>
2020-04-01 22:42:05 -0400

    Update SimpleTable.js

--- src-web/components/common/SimpleTable.js
+++ src-web/components/common/SimpleTable.js
@@ -95,7 +95,7 @@ class StructuredListModule extends React.Component {
                           {Array.isArray(rows) && rows[0].cells.map((cell, index) =>
                             <td key={cell.resourceKey+'Cell'}>
                               <p>{
-                                (linkFixedName && (index in linkFixedName))
+                                (linkFixedName && (index in linkFixedName) && transform(row, cell, this.context.locale) !== '-')
                                   ? <Link to={transform(row, cell, this.context.locale)} target={linkFixedName[index].urlTarget} className='bx--link'>{msgs.get(linkFixedName[index].fixedName, this.context.locale)}</Link>
                                   : cell.link && url ? <Link to={url} className='bx--link'>{transform(row, cell, this.context.locale)}</Link> : transform(row, cell, this.context.locale)
                               }</p>
