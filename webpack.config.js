f1c476a6a0755167be96daf30a7b7605c05ae556
Yu Cao <ycao@redhat.com>
2020-04-01 20:04:48 -0400

    copyright

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,9 @@
  * Note to U.S. Government Users Restricted Rights:
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
- *******************************************************************************/
-
+ *******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ */
 var path = require('path'),
     webpack = require('webpack'),
     ExtractTextPlugin = require('extract-text-webpack-plugin'),
