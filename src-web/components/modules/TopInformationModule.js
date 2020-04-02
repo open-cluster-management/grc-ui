243cf6f20dfbfd9d8576165863059febe7e3f871
Chunxi Luo <Chunxi.Luo@ibm.com>
2020-04-02 10:52:50 -0400

    Copyright

--- src-web/components/modules/TopInformationModule.js
+++ src-web/components/modules/TopInformationModule.js
@@ -6,6 +6,9 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  *******************************************************************************/
+/* Copyright (c) 2020 Red Hat, Inc.
+ */
+
 'use strict'
 
 import React from 'react'
