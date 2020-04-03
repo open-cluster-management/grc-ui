640b26bd179dc7710b1d23d6ab03a75741f96dc6
Chunxi Luo <Chunxi.Luo@ibm.com>
2020-04-02 23:29:13 -0400

    Copyright

--- tests/jest/components/common/FormatTableData.test.js
+++ tests/jest/components/common/FormatTableData.test.js
@@ -6,6 +6,9 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  *******************************************************************************/
+/* Copyright (c) 2020 Red Hat, Inc.
+*/
+
 'use strict'
 
 import { policies, policies2, policies3, findings } from './CommonTestingData'
