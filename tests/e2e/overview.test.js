617ea5ea3065201c8312f7a459d74d2baad767e4
Will-Kutler <will.kutler@ibm.com>
2020-04-13 13:56:52 -0400

    un-comment-out other e2e tests

--- tests/e2e/overview.test.js
+++ tests/e2e/overview.test.js
@@ -1,57 +1,57 @@
-// /*******************************************************************************
-//  * Licensed Materials - Property of IBM
-//  * (c) Copyright IBM Corporation 2018. All Rights Reserved.
-//  *
-//  * Note to U.S. Government Users Restricted Rights:
-//  * Use, duplication or disclosure restricted by GSA ADP Schedule
-//  * Contract with IBM Corp.
-//  *******************************************************************************/
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2018. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ *******************************************************************************/
 
-// const config = require('../../config')
-// // const a11yScan = require('../utils/accessibilityScan')
-// let page
+const config = require('../../config')
+// const a11yScan = require('../utils/accessibilityScan')
+let page
 
-// module.exports = {
-//   '@disabled': true,
+module.exports = {
+  '@disabled': true,
 
-//   before: (browser) => {
-//     const loginPage = browser.page.LoginPage()
-//     loginPage.navigate()
-//     loginPage.authenticate()
+  before: (browser) => {
+    const loginPage = browser.page.LoginPage()
+    loginPage.navigate()
+    loginPage.authenticate()
 
-//     const url = `${browser.launch_url}${config.get('contextPath')}`
-//     page = browser.page.OverviewPage()
-//     page.navigate(url)
-//   },
+    const url = `${browser.launch_url}${config.get('contextPath')}`
+    page = browser.page.OverviewPage()
+    page.navigate(url)
+  },
 
-//   'Overview: Load page': () => {
-//     page.verifyPageLoaded()
-//   },
+  'Overview: Load page': () => {
+    page.verifyPageLoaded()
+  },
 
-//   'Overview: Recent activity': (browser) => {
-//     page.verifyRecentActivity(browser)
-//   },
+  'Overview: Recent activity': (browser) => {
+    page.verifyRecentActivity(browser)
+  },
 
-//   'Overview: Violation table': () => {
-//     page.verifyViolationTable()
-//   },
+  'Overview: Violation table': () => {
+    page.verifyViolationTable()
+  },
 
-//   'Overview: Most impacted controls table': () => {
-//     page.verifyMostImpactedControls()
-//   },
+  'Overview: Most impacted controls table': () => {
+    page.verifyMostImpactedControls()
+  },
 
-//   'Overview: policy summary table': () => {
-//     page.verifyPolicySummary()
-//   },
+  'Overview: policy summary table': () => {
+    page.verifyPolicySummary()
+  },
 
-//   'Overview: Run accessibility scan': () => {
-//     // a11yScan.runAccessibilityScan(browser, 'overview')
-//   },
+  'Overview: Run accessibility scan': () => {
+    // a11yScan.runAccessibilityScan(browser, 'overview')
+  },
 
-//   after: function (browser, done) {
-//     setTimeout(() => {
-//       browser.end()
-//       done()
-//     })
-//   }
-// }
+  after: function (browser, done) {
+    setTimeout(() => {
+      browser.end()
+      done()
+    })
+  }
+}
