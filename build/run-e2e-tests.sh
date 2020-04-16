2264fb402b11b26731d499e283c992fb1e01f240
Yu Cao <ycao@redhat.com>
2020-04-16 13:09:54 -0400

    fix env

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -20,6 +20,6 @@ docker run --network test-network -d --ip 10.10.0.5 -t -i -p 4000:4000 --name gr
 docker run --network test-network -d --ip 10.10.0.6 -t -i -p 3000:3000 --name grcui -e NODE_ENV=development -e SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN -e headerUrl=$headerUrl -e OAUTH2_REDIRECT_URL=$OAUTH2_REDIRECT_URL -e grcUiApiUrl=https://10.10.0.5:4000/grcuiapi -e OAUTH2_CLIENT_ID=$OAUTH2_CLIENT_ID -e OAUTH2_CLIENT_SECRET=$OAUTH2_CLIENT_SECRET -e API_SERVER_URL=$OC_CLUSTER_URL $UI_CURRENT_IMAGE
 docker container ls -a
 
-if [TRAVIS == "true"] npm i chromedriver@73
+if [$TRAVIS == "true"] npm i chromedriver@73
 
 npm run test:e2e
