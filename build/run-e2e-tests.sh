bf49ac33c727a00fe024e9084ad062de86d00e2c
Yu Cao <ycao@redhat.com>
2020-05-22 19:01:43 -0400

    clean up

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -39,8 +39,6 @@ make oc/login
 export SERVICEACCT_TOKEN=`${BUILD_HARNESS_PATH}/vendor/oc whoami --show-token`
 echo "SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN"
 
-# docker network create --subnet 10.10.0.0/16 test-network
-
 make docker/login
 export DOCKER_URI=quay.io/open-cluster-management/grc-ui-api:latest-dev
 make docker/pull
@@ -50,8 +48,10 @@ export SELENIUM_PASSWORD=$OC_HUB_CLUSTER_PASS
 
 ls $(pwd)/test-output -al
 
+# docker network create --subnet 10.10.0.0/16 test-network
+# docker run --network test-network -d --ip 10.10.0.5 -t -i -p 4000:4000 --name grcuiapi -e NODE_ENV=development -e SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN -e API_SERVER_URL=$OC_HUB_CLUSTER_URL $DOCKER_URI
+# docker run --network test-network -d --ip 10.10.0.6 -t -i -p 3000:3000 --name grcui -e NODE_ENV=development -e SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN -e headerUrl=$headerUrl -e OAUTH2_REDIRECT_URL=$OAUTH2_REDIRECT_URL -e grcUiApiUrl=https://10.10.0.5:4000/grcuiapi -e OAUTH2_CLIENT_ID=$OAUTH2_CLIENT_ID -e OAUTH2_CLIENT_SECRET=$OAUTH2_CLIENT_SECRET -e API_SERVER_URL=$OC_HUB_CLUSTER_URL $UI_CURRENT_IMAGE
 docker run -d -t -i -p 4000:4000 --name grcuiapi -e NODE_ENV=development -e SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN -e API_SERVER_URL=$OC_HUB_CLUSTER_URL $DOCKER_URI
-# docker run -d -t -i -p 3000:3000 --name grcui -e NODE_ENV=development -e SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN -e headerUrl=$headerUrl -e OAUTH2_REDIRECT_URL=$OAUTH2_REDIRECT_URL -e OAUTH2_CLIENT_ID=$OAUTH2_CLIENT_ID -e OAUTH2_CLIENT_SECRET=$OAUTH2_CLIENT_SECRET -e API_SERVER_URL=$OC_HUB_CLUSTER_URL $UI_CURRENT_IMAGE
 export NODE_ENV=development 
 # export SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN 
 # export headerUrl=$headerUrl 
