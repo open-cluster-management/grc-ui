a12d8def66e31e50f867c83398101c809362e395
Xuning Kang <Xuning.Kang@ibm.com>
2020-06-30 16:20:24 +0800

    Build test

--- Makefile
+++ Makefile
@@ -11,6 +11,8 @@ include build/Configfile
 
 USE_VENDORIZED_BUILD_HARNESS ?=
 
+TEST_IMAGE_TAG ?= $(COMPONENT_VERSION)$(COMPONENT_TAG_EXTENSION)
+
 ifndef USE_VENDORIZED_BUILD_HARNESS
 -include $(shell curl -s -H 'Authorization: token ${GITHUB_TOKEN}' -H 'Accept: application/vnd.github.v4.raw' -L https://api.github.com/repos/open-cluster-management/build-harness-extensions/contents/templates/Makefile.build-harness-bootstrap -o .build-harness-bootstrap; echo .build-harness-bootstrap)
 else
@@ -45,3 +47,16 @@ travis-slack-reporter:
 	if [ -d "test-output/e2e/screenshots" ]; then \
 		node ./tests/utils/slack-reporter.js; \
 	fi
+
+build-test-image:
+	@echo "Building $(COMPONENT_DOCKER_REPO)/$(COMPONENT_NAME)-tests:$(TEST_IMAGE_TAG)"
+	docker build . \
+	-f Dockerfile.e2etest \
+	-t $(COMPONENT_DOCKER_REPO)/$(COMPONENT_NAME)-tests:$(TEST_IMAGE_TAG)
+
+push-test-image:
+	make component/push COMPONENT_NAME=$(COMPONENT_NAME)-tests
+
+publish-test-image:
+	rm -rf pipeline
+	make pipeline-manifest/update COMPONENT_NAME=$(COMPONENT_NAME)-tests PIPELINE_MANIFEST_COMPONENT_SHA256=${TRAVIS_COMMIT} PIPELINE_MANIFEST_COMPONENT_REPO=${TRAVIS_REPO_SLUG} PIPELINE_MANIFEST_BRANCH=${TRAVIS_BRANCH}
---
+++ build/download-clis.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Copyright (c) 2020 Red Hat, Inc.
+
+mkdir clis-unpacked
+
+# Install OpenShift and Kubectl CLI.
+echo "Installing oc and kubectl clis..."
+curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.4.3/openshift-client-linux-4.4.3.tar.gz
+tar -xzf oc.tar.gz -C clis-unpacked
+chmod 755 ./clis-unpacked/oc
+chmod 755 ./clis-unpacked/kubectl
+mv ./clis-unpacked/oc /usr/local/bin/oc
+mv ./clis-unpacked/kubectl /usr/local/bin/kubectl
+
+# Install Kind CLI
+echo "Installing kind cli"
+curl -Lo ./clis-unpacked/kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
+chmod +x ./clis-unpacked/kind
+mv ./clis-unpacked/kind /usr/local/bin/kind
+
+# Install Docker CLI
+echo "Installing docker cli..."
+curl -kLo docker.tgz https://download.docker.com/linux/static/stable/x86_64/docker-18.06.0-ce.tgz
+tar -xzf docker.tgz -C clis-unpacked
+chmod 755 ./clis-unpacked/docker
+mv ./clis-unpacked/docker/* /usr/local/bin/
+
+rm -rf ./clis-unpacked ./oc.tar.gz ./docker.tgz
+
+# Install helm CLI.
+# curl -kLo helm-linux-amd64.tar.gz https://${CLUSTER_IP}:${CLUSTER_PORT}/api/cli/helm-linux-amd64.tar.gz
+# mkdir helm-unpacked
+# tar -xvzf helm-linux-amd64.tar.gz -C helm-unpacked
+# chmod 755 ./helm-unpacked/*/helm
+# sudo mv ./helm-unpacked/*/helm /usr/local/bin/helm
+# rm -rf ./helm-unpacked ./helm-linux-amd64.tar.gz
+# helm init
+
+# Setup helm certs
+# oc get secret helm-tiller-secret -n kube-system -o json | jq -r .data.crt | base64 --decode > ~/.helm/cert.pem
+# oc get secret helm-tiller-secret -n kube-system -o json | jq -r .data.key | base64 --decode > ~/.helm/key.pem
+
+# helm version --tls
+echo 'set up complete'
