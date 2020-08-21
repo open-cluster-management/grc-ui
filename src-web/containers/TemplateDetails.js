c46b54583ac9a38ade9465b115fe3e1ecb25c87b
Yu Cao <ycao@redhat.com>
2020-08-21 08:55:20 -0400

    fix locale

--- src-web/containers/TemplateDetails.js
+++ src-web/containers/TemplateDetails.js
@@ -8,13 +8,14 @@ import { connect } from 'react-redux'
 import { Spinner } from '@patternfly/react-core'
 import { updateSecondaryHeader } from '../actions/common'
 import {getPollInterval} from '../components/common/RefreshTimeSelect'
-import { GRC_REFRESH_INTERVAL_COOKIE, RESOURCE_TYPES } from '../../lib/shared/constants'
+import { GRC_REFRESH_INTERVAL_COOKIE } from '../../lib/shared/constants'
 import msgs from '../../nls/platform.properties'
 import { Query } from 'react-apollo'
 import { PolicyTemplateDetail } from '../../lib/client/queries'
 import PolicyTemplateDetails from '../components/common/PolicyTemplateDetails'
 import Page from '../components/common/Page'
 import resources from '../../lib/shared/resources'
+import { LocaleContext } from '../components/common/LocaleContext'
 
 resources(() => {
   require('../../scss/policy-template-details.scss')
@@ -24,76 +25,50 @@ class TemplateDetails extends React.Component {
   static propTypes = {
     location: PropTypes.object,
     match: PropTypes.object,
-    resourceType: PropTypes.object,
+    // resourceType: PropTypes.object,
     updateSecondaryHeader: PropTypes.func,
   }
 
-  static defaultProps = {
-    resourceType: RESOURCE_TYPES.HCM_COMPLIANCES,
-  }
-
-  static contextTypes = {
-    locale: PropTypes.string
-  }
+  static contextType = LocaleContext
 
   constructor (props) {
     super(props)
   }
 
-  UNSAFE_componentWillMount() {
-    // console.log(this.props)
-    const { updateSecondaryHeader: localUpdateSecondaryHeader, match: { params: { name }} } = this.props
-    localUpdateSecondaryHeader(name, null, this.getBreadcrumb())
-  }
-
   getBreadcrumb() {
     const breadcrumbItems = []
-    const {  location, resourceType } = this.props,
+    const { location } = this.props,
           { locale } = this.context,
           urlSegments = location.pathname.split('/')
-    const { match: { params: { name, policyName, policyNamespace, clusterName, kind }} } = this.props
-    // console.log(this.props)
-
-    // Push only one breadcrumb to overview page
-    if (resourceType.name === RESOURCE_TYPES.HCM_COMPLIANCES.name) {
-      breadcrumbItems.push({
-        label: msgs.get('tabs.hcmcompliance', locale),
-        noLocale: true,
-        url: `${urlSegments.slice(0, 3).join('/')}/all`
-      },
-      {
-        label: policyName,
-        noLocale: true,
-        url: `${urlSegments.slice(0, 3).join('/')}/all/${policyNamespace}/${policyName}`
-      },
-      {
-        label: clusterName,
-        noLocale: true,
-        url: `${urlSegments.join('/')}`
-      },
-      {
-        label: kind.toLowerCase(),
-        noLocale: true,
-        url: `${urlSegments.join('/')}`
-      },
-      {
-        label: name,
-        noLocale: true,
-        url: `${urlSegments.join('/')}`
-      })
-    }
+    const { match: { params: { policyName, policyNamespace, clusterName }} } = this.props
+    breadcrumbItems.push({
+      label: msgs.get('tabs.hcmcompliance', locale),
+      noLocale: true,
+      url: `${urlSegments.slice(0, 3).join('/')}/all`
+    },
+    {
+      label: policyName,
+      noLocale: true,
+      url: `${urlSegments.slice(0, 3).join('/')}/all/${policyNamespace}/${policyName}`
+    },
+    {
+      label: clusterName,
+      noLocale: true,
+      url: `${urlSegments.join('/')}`
+    })
     return breadcrumbItems
   }
 
+  componentDidMount() {
+    const { updateSecondaryHeader: localUpdateSecondaryHeader, match: { params: { name }} } = this.props
+    console.log(name)
+    localUpdateSecondaryHeader(name, null, this.getBreadcrumb())
+  }
+
   render() {
-    // const url = lodash.get(this.props, 'location.pathname')
-    // const urlSegments = url.split('/')
-    // const policyName = urlSegments[urlSegments.length - 1]
-    // const policyNamespace = urlSegments[urlSegments.length - 2]
     const pollInterval = getPollInterval(GRC_REFRESH_INTERVAL_COOKIE)
     const { match: { params: { clusterName: cluster, apiGroup, version, kind, name }}} = this.props
     const selfLink = `/apis/${apiGroup}/${version}/namespaces/${cluster}/${kind}/${name}`
-    // console.log(selfLink)
     return (
       <Query query={PolicyTemplateDetail} variables={{name, cluster, kind, selfLink}} pollInterval={pollInterval} notifyOnNetworkStatusChange >
         {(result) => {
