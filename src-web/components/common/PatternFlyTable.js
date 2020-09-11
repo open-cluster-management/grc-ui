4bc9afd4d4a9d5d5a5328d4df5a1e758f7fbd238
Dale Haiducek <dhaiduce@redhat.com>
2020-09-11 18:03:14 -0400

    Insert search component above table
    Only the component--doesn't filter table contents yet.

--- src-web/components/common/PatternFlyTable.js
+++ src-web/components/common/PatternFlyTable.js
@@ -10,6 +10,7 @@ import {
   EmptyStateIcon,
   Pagination,
   PaginationVariant,
+  SearchInput,
   Title,
 } from '@patternfly/react-core'
 import {
@@ -19,6 +20,7 @@ import {
   SortByDirection
 } from '@patternfly/react-table'
 import { SearchIcon } from '@patternfly/react-icons'
+import msgs from '../../../nls/platform.properties'
 import resources from '../../../lib/shared/resources'
 
 resources(() => {
@@ -35,8 +37,13 @@ class PatternFlyTable extends React.Component {
       itemCount: 0,
       sortBy: this.props.sortBy || {},
       startIdx: 0,
-      endIdx: 9
+      endIdx: 9,
+      searchValue: ''
     }
+  }
+  static defaultProps = {
+    searchable: true,
+    searchPlaceholder: 'table.search.placeholder'
   }
   static getDerivedStateFromProps(props, state) {
     const { sortBy } = state
@@ -71,39 +78,53 @@ class PatternFlyTable extends React.Component {
       endIdx
     })
   }
+  handleSearch = (value) => {
+    this.setState({
+      searchValue: value
+    })
+  }
   render() {
-    const { sortBy, rows=[], itemCount } = this.state
-    const { columns, className, noResultMsg } = this.props
+    const { locale } = this.context
+    const { sortBy, rows=[], itemCount, searchValue } = this.state
+    const { columns, className, noResultMsg, searchable, searchPlaceholder } = this.props
     const classes = classNames('pattern-fly-table', className)
     return (
-      <div className={classes}>
-        <Table aria-label='Sortable Table' sortBy={sortBy} onSort={this.handleSort} cells={columns} rows={rows}>
-          <TableHeader className='pattern-fly-table-header' />
-          <TableBody className='pattern-fly-table-body' />
-        </Table>
-        {rows.length === 0 && (
-          <EmptyState className='pattern-fly-table-empty-state' variant={EmptyStateVariant.small}>
-            <EmptyStateIcon icon={SearchIcon} />
-            <Title headingLevel='h2' size='md'>
-              {noResultMsg}
-            </Title>
-          </EmptyState>
-        )}
-        <Pagination
-          itemCount={itemCount}
-          widgetId='pagination-options-menu-bottom'
-          perPage={this.state.perPage}
-          page={this.state.page}
-          variant={PaginationVariant.bottom}
-          onSetPage={this.handleSetPage}
-          onPerPageSelect={this.handlePerPageSelect}
-          perPageOptions={[
-            { title: '5', value: 5 },
-            { title: '10', value: 10 },
-            { title: '20', value: 20},
-            { title: '50', value: 50},
-          ]}
-        />
+      <div className='pattern-fly-table-group'>
+        {searchable && <SearchInput
+          placeholder={msgs.get(searchPlaceholder, locale)}
+          value={searchValue}
+          onChange={this.handleSearch}
+          onClear={() => this.handleSearch('')}
+        />}
+        <div className={classes}>
+          <Table aria-label='Sortable Table' sortBy={sortBy} onSort={this.handleSort} cells={columns} rows={rows}>
+            <TableHeader className='pattern-fly-table-header' />
+            <TableBody className='pattern-fly-table-body' />
+          </Table>
+          {rows.length === 0 && (
+            <EmptyState className='pattern-fly-table-empty-state' variant={EmptyStateVariant.small}>
+              <EmptyStateIcon icon={SearchIcon} />
+              <Title headingLevel='h2' size='md'>
+                {noResultMsg}
+              </Title>
+            </EmptyState>
+          )}
+          <Pagination
+            itemCount={itemCount}
+            widgetId='pagination-options-menu-bottom'
+            perPage={this.state.perPage}
+            page={this.state.page}
+            variant={PaginationVariant.bottom}
+            onSetPage={this.handleSetPage}
+            onPerPageSelect={this.handlePerPageSelect}
+            perPageOptions={[
+              { title: '5', value: 5 },
+              { title: '10', value: 10 },
+              { title: '20', value: 20},
+              { title: '50', value: 50},
+            ]}
+          />
+        </div>
       </div>
     )
 
@@ -115,6 +136,8 @@ PatternFlyTable.propTypes = {
   columns: PropTypes.array,
   noResultMsg: PropTypes.string,
   rows: PropTypes.array,
+  searchPlaceholder: PropTypes.string,
+  searchable: PropTypes.bool,
   sortBy: PropTypes.shape({
     index: PropTypes.number,
     direction: PropTypes.oneOf(['asc', 'desc']),
