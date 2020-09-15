a2779a8a9d8e8ed31aafaef5f98dce241bbdbd0e
Will <willkutler@gmail.com>
2020-09-15 08:37:27 -0600

    clean up dead transform code

--- src-web/tableDefinitions/violationHistoryDef.js
+++ src-web/tableDefinitions/violationHistoryDef.js
@@ -2,14 +2,11 @@
 
 'use strict'
 
-import React from 'react'
-import lodash from 'lodash'
 import {
   breakWord,
   sortable,
   wrappable,
 } from '@patternfly/react-table'
-import msgs from '../../nls/platform.properties'
 
 export default {
   tableKeys: [
@@ -24,21 +21,9 @@ export default {
       resourceKey: 'timestamp',
       transforms: [sortable, wrappable],
     },
-    {
-      transformFunction: buildViewYamlLink
-    },
   ],
   sortBy: {
     index: 1,
     direction: 'asc',
   }
-}
-
-function buildViewYamlLink(item, locale) {
-  const selfLink = lodash.get(item, 'object.metadata.selfLink')
-  if (selfLink) {
-    return <a target='_blank' rel='noopener noreferrer'
-      href={`/multicloud/details/${lodash.get(item, 'cluster')}${selfLink}`}>{msgs.get('table.actions.view.yaml', locale)}</a>
-  }
-  return ''
 }
