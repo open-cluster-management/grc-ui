59c089a93f5efeecdb6d1b3b2f5b22e18c3398c2
Yu Cao <ycao@redhat.com>
2020-09-30 16:36:37 -0400

    fix typo

--- build/install-cert-manager.sh
+++ build/install-cert-manager.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2020 Red Hat, Inc.
 
 if [ $(oc get ns cert-manager | grep Active | wc -l | tr -d '[:space:]') -eq 1 ]; then
-    echo "Cert manager alreayd installed"
+    echo "Cert manager already installed"
 else 
     echo "Install cert manager on managed"
     oc apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.1/cert-manager.yaml
