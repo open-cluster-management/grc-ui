ab935ce482cf6f55df0878fa4596999ffeeadcc7
Karel Srot <ksrot@redhat.com>
2020-11-30 23:30:19 +0100

    avoid repetitive application of formatResourceName()

--- tests/cypress/scripts/utils.js
+++ tests/cypress/scripts/utils.js
@@ -11,7 +11,7 @@ export const getOpt = (opts, key, defaultValue) => {
 
 // create an unique resource name
 export const formatResourceName = name => {
-  if (Cypress.env('RESOURCE_ID'))
+  if (Cypress.env('RESOURCE_ID') && (!name.endsWith(Cypress.env('RESOURCE_ID'))))
     name = `${name}-${Cypress.env('RESOURCE_ID')}`
 
   return name
