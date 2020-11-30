281813f99f5cbae667869650f800245b137b5c34
ChunxiAlexLuo <55596988+ChunxiAlexLuo@users.noreply.github.com>
2020-11-30 17:52:09 -0500

    fix lint

--- tests/cypress/config/index.js
+++ tests/cypress/config/index.js
@@ -1,8 +1,4 @@
-/** *****************************************************************************
- * Licensed Materials - Property of Red Hat, Inc.
- * Copyright (c) 2020 Red Hat, Inc.
- ****************************************************************************** */
-
+/* Copyright (c) 2020 Red Hat, Inc. */
 const fs = require('fs')
 const jsYaml = require('js-yaml')
 
--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,26 +1,13 @@
-/** *****************************************************************************
- * Licensed Materials - Property of Red Hat, Inc.
- * Copyright (c) 2020 Red Hat, Inc.
- ****************************************************************************** */
+/* Copyright (c) 2020 Red Hat, Inc. */
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
 
 /**
  * @type {Cypress.PluginConfig}
  */
 
-const glob = require("glob")
-const path = require("path")
+const glob = require('glob')
+const path = require('path')
 const getConfig = require('../config').getConfig
-
 const configFiles = glob.sync(path.join(__dirname, '../config', '*.yaml'))
 
 module.exports = (on, config) => {
