33ed236abf2c0a35297dd8f445eab1c599aa6bfe
Karel Srot <ksrot@redhat.com>
2020-12-01 11:26:14 +0100

    fixed lint errors

--- tests/cypress/tests/yaml-editor-demo.spec.js
+++ tests/cypress/tests/yaml-editor-demo.spec.js
@@ -10,19 +10,19 @@ describe('YAML editor demo', () => {
   it ('can test YAML editor content', () => {
     cy.visit('/multicloud/policies/create')
     //cy.YAMLeditor("inmemory://model/1").invoke('getValue').should('contain', 'apiVersion: policy.open-cluster-management.io/v1')
-      .toggleYAMLeditor("On")
+      .toggleYAMLeditor('On')
       .YAMLeditor()
       .invoke('getValue')
       .should('contain', 'apiVersion: policy.open-cluster-management.io/v1')
-      .toggleYAMLeditor("Off")
-      .wait(200)
+      .toggleYAMLeditor('Off')
+      //.wait(1000)  // just to give user a time to observe the change
       .toggleYAMLeditor()
       .YAMLeditor()
       .invoke('setValue', 'foo: bar')
-      .wait(200)
+      //.wait(1000)  // just to give user a time to observe the change
 
     cy.visit('multicloud/clusters/create/ocp')
-      .toggleYAMLeditor("On")
+      .toggleYAMLeditor('On')
       .YAMLeditor()
       .invoke('getValue')
       .should('contain', 'apiVersion: hive.openshift.io/v1')
