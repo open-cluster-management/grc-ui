d62c1e929ffbadb7c98637db412a51262b88599b
ChunxiAlexLuo <55596988+ChunxiAlexLuo@users.noreply.github.com>
2020-12-08 11:03:05 -0500

    save

--- tests/cypress/config/index.js
+++ tests/cypress/config/index.js
@@ -3,15 +3,23 @@ const fs = require('fs')
 const jsYaml = require('js-yaml')
 
 exports.getConfig = (filepath) => {
-  let config = fs.readFileSync(filepath)
   try {
-    config = jsYaml.safeLoad(config)
+    return fs.readFileSync(filepath).toString()
   } catch (e) {
     throw new Error(e)
   }
-  return JSON.stringify(config)
 }
 
-exports.getConfigObject = (prefix) => {
-  return JSON.parse(Cypress.env('TEST_CONFIG_'+prefix.toUpperCase()))
+exports.getConfigObject = (postfix, returnType='raw') => {
+  try {
+    switch(returnType.toLowerCase()) {
+      case 'json':
+        return jsYaml.load(Cypress.env('TEST_CONFIG_'+postfix.replace('-','_').toUpperCase()))
+      case 'raw':
+      default:
+        return Cypress.env('TEST_CONFIG_'+postfix.replace('-','_').toUpperCase())
+    }
+  } catch (e) {
+    throw new Error(e)
+  }
 }
--- tests/cypress/views/policy.js
+++ tests/cypress/views/policy.js
@@ -1,5 +1,6 @@
 /* Copyright (c) 2020 Red Hat, Inc. */
 /// <reference types="cypress" />
+import { selectItems } from './common'
 
 export const createPolicyFromYAML = (uPolicyName, policyYAML, create=false) => {
   let label = '[]'
@@ -22,6 +23,64 @@ export const createPolicyFromYAML = (uPolicyName, policyYAML, create=false) => {
         cy.get('#create-button-portal-id-btn').click()
       }
     })
+    // after creation, always return to grc main page
+    cy.CheckGrcMainPage()
+}
+
+// this function is mainly used to testing selection on the create policy page
+export const createPolicyFromSelection = ({ uPolicyName, create=false, ...policyConfig }) => {
+  // fill the form uName
+  cy.get('input[aria-label="name"]')
+    .clear()
+    .type(uPolicyName)
+  // namespace
+  cy.get('.bx--dropdown[aria-label="Choose an item"]')
+    .click()
+    .contains(policyConfig['namespace'])
+    .click()
+  //specs
+    .then(() => {
+      selectItems(policyConfig['specifications'], '.bx--multi-select[aria-label="specs"]')
+    })
+  // cluster binding
+    .then(() => {
+      selectItems(policyConfig['cluster_binding'], '.bx--multi-select[aria-label="clusters"]', )
+    })
+  // standards
+    .then(() => {
+      selectItems(policyConfig['standards'], '.bx--multi-select[aria-label="standards"]', )
+    })
+  // categories
+    .then(() => {
+      selectItems(policyConfig['categories'], '.bx--multi-select[aria-label="categories"]', )
+    })
+  // controls
+    .then(() => {
+      selectItems(policyConfig['controls'], '.bx--multi-select[aria-label="controls"]', )
+    })
+  // enforce
+    .then(() => {
+      if (policyConfig['enforce']) {
+        cy.get('input[aria-label="enforce"][type="checkbox"]')
+          .next('label')
+          .click()
+      }
+    })
+  // disable
+    .then(() => {
+      if (policyConfig['disable']) {
+        cy.get('input[aria-label="disabled"][type="checkbox"]')
+          .next('label')
+          .click()
+      }
+    })
+  // create
+    .then(() => {
+      if (create) {
+        cy.get('#create-button-portal-id-btn').click()
+      }
+    })
+
     // after creation, always return to grc main page
     cy.CheckGrcMainPage()
 }
