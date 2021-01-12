1939dc20705b6f478e88752ccd0ac56d41cd121a
Karel Srot <ksrot@redhat.com>
2021-01-12 11:17:19 +0100

    fixed function call

--- tests/cypress/tests/Role_policy_governance.spec.js
+++ tests/cypress/tests/Role_policy_governance.spec.js
@@ -3,7 +3,9 @@
 import { test_genericPolicyGovernance } from './common/generic_policies_governance'
 import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup'
 
-test_genericPolicyGovernance('Role policy governance', 'Role_policy_governance/policy-config.yaml', 'Role_policy_governance/violations-inform.yaml', 'Role_policy_governance/violations-enforce.yaml')
+describe('Role policy governance', () => {
+  test_genericPolicyGovernance('Role_policy_governance/policy-config.yaml', 'Role_policy_governance/violations-inform.yaml', 'Role_policy_governance/violations-enforce.yaml')
+})
 
 describe('Role policy governance - clean up', () => {
   cleanup_usingPolicyYAML('Role_policy_governance/role_specification_cleanup_policy_raw.yaml')
