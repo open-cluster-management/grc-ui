fcf0bb2607abb10eeaf25f8e2b3b513b14523dad
anamdhar <anamdhar@redhat.com>
2021-01-12 21:53:47 +0530

    Added violation patterns & updated specs

--- tests/cypress/tests/Namespace_governance.spec.js
+++ tests/cypress/tests/Namespace_governance.spec.js
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 Red Hat, Inc. */
 /* Copyright (c) 2020 Red Hat, Inc. */
 /// <reference types="cypress" />
-import { test_genericPolicyGovernance } from './common/generic_policies_governance'
-import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup'
+import { test_genericPolicyGovernance } from './common/generic_policies_governance.spec.js'
+import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup.js'
 
 test_genericPolicyGovernance('Namespace governance', 'Namespace_governance/policy-config.yaml', 'Namespace_governance/violations-inform.yaml', 'Namespace_governance/violations-enforce.yaml')
 describe('Namespace policy governance - clean up', () => {
--- tests/cypress/tests/Pod_governance.spec.js
+++ tests/cypress/tests/Pod_governance.spec.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2020 Red Hat, Inc. */
 /// <reference types="cypress" />
-import { test_genericPolicyGovernance } from './common/generic_policies_governance.spec'
-import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup'
+import { test_genericPolicyGovernance } from './common/generic_policies_governance.spec.js'
+import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup.js'
 
 test_genericPolicyGovernance('Pod governance', 'Pod_governance/policy-config.yaml', 'Pod_governance/violations-inform.yaml', 'Pod_governance/violations-enforce.yaml')
 describe('Pod policy governance - clean up', () => {
