1047eda89867ca9a0f9eda51c8f3097d37fa92ff
anamdhar <anamdhar@redhat.com>
2021-01-12 20:22:02 +0530

    Fixed lint & import issues

--- tests/cypress/tests/ImageManifest_governance.spec.js
+++ tests/cypress/tests/ImageManifest_governance.spec.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2020 Red Hat, Inc. */
 /// <reference types="cypress" />
-import { test_genericPolicyGovernance } from './common/generic_policies_governance.spec.js'
-import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup.js'
+import { test_genericPolicyGovernance } from './common/generic_policies_governance'
+import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup'
 
 test_genericPolicyGovernance('ImageManifest governance', 'ImageManifest_governance/policy-config.yaml', 'ImageManifest_governance/violations-inform.yaml', 'ImageManifest_governance/violations-enforce.yaml')
 describe('ImageManifest policy governance - clean up', () => {
