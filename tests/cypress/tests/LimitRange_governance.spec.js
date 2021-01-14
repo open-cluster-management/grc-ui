39815bcd65a75957d7373583f9708e0c8eaaa10c
anamdhar <anamdhar@redhat.com>
2021-01-14 14:10:40 +0530

    Added setup files for namespace & limitrange

--- tests/cypress/tests/LimitRange_governance.spec.js
+++ tests/cypress/tests/LimitRange_governance.spec.js
@@ -3,6 +3,10 @@
 import { test_genericPolicyGovernance } from './common/generic_policies_governance'
 import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup'
 
+describe('LimitRange policy governance - setup', () => {
+    cleanup_usingPolicyYAML('LimitRange_governance/limitRange_specification_setup_policy_raw.yaml')
+})
+
 describe('LimitRange policy governance', () => {
     test_genericPolicyGovernance('LimitRange_governance/policy-config.yaml', 'LimitRange_governance/violations-inform.yaml', 'LimitRange_governance/violations-enforce.yaml')
 })
--- tests/cypress/tests/Namespace_governance.spec.js
+++ tests/cypress/tests/Namespace_governance.spec.js
@@ -3,6 +3,9 @@
 import { test_genericPolicyGovernance } from './common/generic_policies_governance'
 import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup'
 
+describe('Namespace policy governance - setup', () => {
+    cleanup_usingPolicyYAML('Namespace_governance/namespace_specification_setup_policy_raw.yaml')
+})
 describe('Namespace policy governance', () => {
     test_genericPolicyGovernance('Namespace_governance/policy-config.yaml', 'Namespace_governance/violations-inform.yaml', 'Namespace_governance/violations-enforce.yaml')
 })
