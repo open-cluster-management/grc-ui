22eaaa2175b7155771d6e91db26a48608411ecf6
anamdhar <anamdhar@redhat.com>
2021-01-16 14:19:33 +0530

    Updated the specs file

--- tests/cypress/tests/ImageManifest_governance.spec.js
+++ tests/cypress/tests/ImageManifest_governance.spec.js
@@ -3,10 +3,10 @@
 import { test_genericPolicyGovernance } from './common/generic_policies_governance'
 import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup'
 
-describe('GRC UI: [P1][Sev1][policy-grc] ImageManifest policy governance', () => {
+describe('RHACM4K-1727 - GRC UI: [P1][Sev1][policy-grc] ImageManifest policy governance', () => {
     test_genericPolicyGovernance('ImageManifest_governance/policy-config.yaml', 'ImageManifest_governance/violations-inform.yaml', 'ImageManifest_governance/violations-enforce.yaml')
 })
-
-describe('RHACM4K-1727 - GRC UI: [P1][Sev1][policy-grc] ImageManifest policy governance - clean up', () => {
+/* 
+describe('GRC UI: [P1][Sev1][policy-grc] ImageManifest policy governance - clean up', () => {
     cleanup_usingPolicyYAML('ImageManifest_governance/ImageManifest_specification_cleanup_policy_raw.yaml')
-})
+})*/
