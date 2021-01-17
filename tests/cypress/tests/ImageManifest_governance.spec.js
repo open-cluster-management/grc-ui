58158eadb3aa037e59f011fbaebfc26adb90924a
Abhijit Namdhari <34916415+abhiNamdhari@users.noreply.github.com>
2021-01-17 22:11:58 +0530

    Delete ImageManifest_governance.spec.js

--- tests/cypress/tests/ImageManifest_governance.spec.js
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2020 Red Hat, Inc. */
-/// <reference types="cypress" />
-import { test_genericPolicyGovernance } from './common/generic_policies_governance'
-
-describe('RHACM4K-1727 - GRC UI: [P1][Sev1][policy-grc] ImageManifest policy governance', () => {
-    test_genericPolicyGovernance('ImageManifest_governance/policy-config.yaml', 'ImageManifest_governance/violations-inform.yaml', 'ImageManifest_governance/violations-enforce.yaml')
-})
-
