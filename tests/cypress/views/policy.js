521279d0d082bca796fc24a35ef2ce66fd9a577f
anamdhar <anamdhar@redhat.com>
2021-01-18 22:05:07 +0530

    resolved confilicts

--- tests/cypress/views/policy.js
+++ tests/cypress/views/policy.js
@@ -455,7 +455,7 @@ export const getPolicyTemplatesNameAndKind = (policyName, policyConfig) => {
       templates.add(policyName+'-limit-clusteradmin'+'/'+'IamPolicy')
       break
     case 'ImageManifestVulnPolicy':
-      templates.add(policyName+'-image-vulnerabilities'+'/'+'ConfigurationPolicy')
+      templates.add(policyName+'-image-vulnerability'+'/'+'ConfigurationPolicy')
       templates.add(policyName+'-image-vulnerability-sub'+'/'+'ConfigurationPolicy')
       break
     case 'LimitRange':
