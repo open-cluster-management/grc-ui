2b8b36f14679a01c8899ff4a924a964c197cc815
anamdhar <anamdhar@redhat.com>
2021-01-18 17:14:14 +0530

    Commited the changes as per comments

--- tests/cypress/tests/LimitRange_governance.spec.js
+++ tests/cypress/tests/LimitRange_governance.spec.js
@@ -10,7 +10,7 @@ describe('GRC UI: [P1][Sev1][policy-grc] LimitRange policy governance - setup', 
 describe('RHACM4K-1726 - GRC UI: [P1][Sev1][policy-grc] LimitRange policy governance', () => {
     test_genericPolicyGovernance('LimitRange_governance/policy-config.yaml', 'LimitRange_governance/violations-inform.yaml', 'LimitRange_governance/violations-enforce.yaml')
 })
-
+//setup & clean up yaml files are identical
 describe('GRC UI: [P1][Sev1][policy-grc] LimitRange policy governance - clean up', () => {
-    cleanup_usingPolicyYAML('LimitRange_governance/LimitRange_specification_cleanup_policy_raw.yaml')
+    cleanup_usingPolicyYAML('LimitRange_governance/LimitRange_specification_setup_policy_raw.yaml')
 })
