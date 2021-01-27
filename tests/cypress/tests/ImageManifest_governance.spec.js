5eeb2dff1530438dd5daa15b44e6876fe673deb2
Yu Cao <ycao@redhat.com>
2021-01-27 16:12:31 -0500

    Add clean up and retry for entire test case

--- tests/cypress/tests/ImageManifest_governance.spec.js
+++ tests/cypress/tests/ImageManifest_governance.spec.js
@@ -5,9 +5,16 @@ import { getClusterListByVendor } from '../config'
 
 const getList = getClusterListByVendor('OpenShift')
 
-if(getList.length>0)
-{
-    describe('RHACM4K-1727 - GRC UI: [P1][Sev1][policy-grc] ImageManifest policy governance', () => {
-        test_genericPolicyGovernance('ImageManifest_governance/policy-config.yaml', 'ImageManifest_governance/violations-inform.yaml', 'ImageManifest_governance/violations-enforce.yaml', 'clusters.yaml', getList)
-    })
+if (getList.length>0) {
+  describe('RHACM4K-1727 - GRC UI: [P1][Sev1][policy-grc] ImageManifest policy governance', {
+    retries: {
+      runMode: 2,
+      openMode: 2,
+  }
+}, () => {
+  test_genericPolicyGovernance('ImageManifest_governance/policy-config.yaml', 'ImageManifest_governance/violations-inform.yaml', 'ImageManifest_governance/violations-enforce.yaml', 'clusters.yaml', getList)
+})
+  describe('GRC UI: [P1][Sev1][policy-grc] ImageManifest governance - cleanup', () => {
+    cleanup_usingPolicyYAML('ImageManifest_governance/ImageManifest_specification_cleanup_policy_raw.yaml')
+  })
 }
