f18bae99d4f72a38267fa1ee7a6df13237cae62d
Yu Cao <ycao@redhat.com>
2021-02-03 09:33:11 -0500

    fix

--- build/cluster-clean-up.sh
+++ build/cluster-clean-up.sh
@@ -22,7 +22,7 @@ function managed() {
     oc delete subscriptions.operators.coreos.com container-security-operator -n openshift-operators || true
     oc delete ClusterServiceVersion -n openshift-operators container-security-operator.v3.3.4 || true
     oc delete crd imagemanifestvulns.secscan.quay.redhat.com || true
-    oc delete LimitRange container-mem-limit-range || true
+    oc delete LimitRange container-mem-limit-range -n default || true
     oc delete ns prod || true
     oc delete psp restricted-psp || true
     oc delete role deployments-role -n default || true
