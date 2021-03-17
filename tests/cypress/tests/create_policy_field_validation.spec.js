c05ad28b2df5c150d0558e9ccaf8295e62d7992f
Yu Cao <ycao@redhat.com>
2021-03-17 10:40:25 -0400

    update test case

--- tests/cypress/tests/create_policy_field_validation.spec.js
+++ tests/cypress/tests/create_policy_field_validation.spec.js
@@ -12,13 +12,11 @@ describe('RHACM4K-2349 - GRC UI: [P1][Sev1][policy-grc] Create policy page: Chec
                          ['-this-is-not-a-valid-name', 'default', errorMsg2], // starts with dash
                          ['this-is-not-a-valid-name-', 'default', errorMsg],  // ends with dash
                          ['this-is-Not-a-valid-name-', 'default', errorMsg],  // contain uppercase letter
-                         // please remove the 'o' prefix once bz#1939409 is fixed, length is now 1 chars more due to a bug
-                         ['othis-is-57-characters-long-policy-name-xxxxxxxxxxxxxxxxxx', 'default', errorMsg],  // name+namespace = 64 characters (now 65)
-                         ['othis-is-60-characters-long-policy-name-xxxxxxxxxxxxxxxxxxxxx', 'hive', errorMsg],  // name+namespace = 64 characters (now 65)
-                         ['othis-is-41-characters-long-policy-name-xx', 'open-cluster-management', errorMsg],  // name+namespace = 64 characters (now 65)
-                         ['this-is-'+'very-'.repeat(20)+'long-policy-name', 'default', errorMsg],  // name+namespace is really long
+                         ['this-is-56-characters-long-policy-name-xxxxxxxxxxxxxxxxx', 'default', errorMsg],  // name.namespace = 64 characters
+                         ['this-is-59-characters-long-policy-name-xxxxxxxxxxxxxxxxxxxx', 'hive', errorMsg],  // name.namespace = 64 characters
+                         ['this-is-40-characters-long-policy-name-x', 'open-cluster-management', errorMsg],  // name.namespace = 64 characters
+                         ['this-is-'+'very-'.repeat(20)+'long-policy-name', 'default', errorMsg],  // name.namespace is really long
                        ]
-  //const longestValidName = 'this-is-56-characters-long-policy-name-xxxxxxxxxxxxxxxxx'  // again, see bz#1939409#c1
   const longestValidName = 'this-is-55-characters-long-policy-name-xxxxxxxxxxxxxxxx'
 
   it('Check that invalid policy name pattern issues an error', () => {
