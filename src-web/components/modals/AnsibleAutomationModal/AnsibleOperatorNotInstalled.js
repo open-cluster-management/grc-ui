15b2eebb1e085e656814f52f594ea583051ef6f9
ChunxiAlexLuo <55596988+ChunxiAlexLuo@users.noreply.github.com>
2021-06-22 09:06:08 -0400

    save
    Signed-off-by: Chunxi Luo chuluo@redhat.com
    Signed-off-by: ChunxiAlexLuo <55596988+ChunxiAlexLuo@users.noreply.github.com>

--- src-web/components/modals/AnsibleAutomationModal/AnsibleOperatorNotInstalled.js
+++ src-web/components/modals/AnsibleAutomationModal/AnsibleOperatorNotInstalled.js
@@ -8,26 +8,13 @@ import { AcmLaunchLink } from '@open-cluster-management/ui-components'
 import { Alert } from '@patternfly/react-core'
 import msgs from '../../../nls/platform.properties'
 import '../../../scss/ansible-modal.scss'
+import { getAnsibleOperatorLink } from '../../../../server/lib/server/ansibleOperatorLink'
 
 export const renderAnsibleOperatorNotInstalled = (locale) => {
   const link = {
     id: 'installAnsibleOperatorLink',
     text: msgs.get('ansible.operator.installLink', locale),
-    onClick: () => {
-      fetch('/multicloud/api/v1/namespaces/openshift-config-managed/configmaps/console-public/')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.statusText)
-        }
-        return response.json()
-      })
-      .then((respJSON) => {
-        window.open(respJSON.data.consoleURL + '/operatorhub/all-namespaces?keyword=ansible+automation+platform')
-      })
-      .catch((err) => {
-        console.error(err)
-      })
-    }
+    onClick: getAnsibleOperatorLink,
   }
   return (
     <Alert
