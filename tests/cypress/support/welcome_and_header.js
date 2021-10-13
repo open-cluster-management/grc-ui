774020005cc74ee0c81a8f76e8f778eecdcb90bd
ChunxiAlexLuo <55596988+ChunxiAlexLuo@users.noreply.github.com>
2021-10-13 10:04:18 -0400

    fix grc toggle click for cypress dom detached
    Signed-off-by: Chunxi Luo chuluo@redhat.com

--- tests/cypress/support/welcome_and_header.js
+++ tests/cypress/support/welcome_and_header.js
@@ -158,7 +158,7 @@ export const userMenu = {
         cy.waitUntil(() => {
             return cy.get('[data-test="app-dropdown"]').should('be.visible')
         }, {'interval': 1000, 'timeout':120000})
-        cy.get('[data-test="app-dropdown"]').should('be.visible').click()
+        cy.get('[data-test="app-dropdown"]').should('be.visible').click({ force: true })
         // seems the split line is also li
         cy.get('[data-test="app-dropdown"] li').should('be.visible').and('have.length', 5)
         cy.contains('Red Hat applications').should('be.visible')
@@ -166,7 +166,7 @@ export const userMenu = {
         cy.contains('OpenShift GitOps').should('be.visible')
         cy.contains('ArgoCD').should('be.visible')
         cy.waitUntil(() => cy.get('[data-test="app-dropdown"]').should('be.visible'))
-        cy.get('[data-test="app-dropdown"]').should('be.visible').click()
+        cy.get('[data-test="app-dropdown"]').should('be.visible').click({ force: true })
         cy.contains('ArgoCD').should('not.exist')
     },
     openAppsNoArgo: () => {
@@ -177,7 +177,7 @@ export const userMenu = {
         cy.waitUntil(() => {
             return cy.get('[data-test="app-dropdown"]').should('be.visible')
         }, {'interval': 1000, 'timeout':120000})
-        cy.get('[data-test="app-dropdown"]').should('be.visible').click()
+        cy.get('[data-test="app-dropdown"]').should('be.visible').click({ force: true })
         cy.contains('Red Hat applications').should('be.visible')
         cy.contains('Red Hat Openshift Container Platform').should('be.visible')
     }
